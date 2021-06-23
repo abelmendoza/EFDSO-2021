c5875ebd720cd797bf890b0634ccf70cd9867ea9
ABEL MENDOZA <mendozaabel66@gmail.com>
2021-06-23 19:51:16 -0400

    Added to controllers product ,user and DBContext

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiVentas.Context;
+
+
+namespace ApiVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbcontext context;
+        public UsuarioController(AppDbcontext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(context.usuario.ToList());
+        }
+
+    }
+}
